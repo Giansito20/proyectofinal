dc7960d9f4f3663250c443bce6a32634e2dddbcb
¨gian¨ <hgian.97@gmail.com>
2022-04-11 20:01:44 -0300

    deploy

---
+++ index.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const app = express();
+const Port = 3000 || 8080;
+const path = require('path');
+const hbs = require('hbs');
+//Traemos la librería para la conexión
+const mysql = require('mysql2');
+
+//Creamos la configuración de la conexión
+const conexion =  mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Hakunamatata20",
+    database: "proyectofinal",
+});
+
+//Conectamos a la DB
+conexion.connect((error) =>{
+    if(error) throw error;
+    console.log('Conexión a la Data Base exitosa!!');
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'))
+
+//Configuramos el Motor de Plantillas
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+
+app.get('/', (req, res) =>{
+    res.render('index', {titulo: 'Bienvenidos a Alabama Beer'})
+});
+
+app.get('/formulario', (req, res) =>{
+    res.render('formulario', {titulo: 'Haz tu pedido'})
+});
+
+app.post('/formulario', (req, res) =>{
+
+    //Desestructuración de las variables
+    const { nombre, precio,} = req.body;
+        
+    if(nombre == "" || precio == ""){
+        
+        let validacion = 'Faltan datos para guardar el Pedido';
+        
+        res.render('formulario', {
+            titulo: 'Haz tu pedido',
+            validacion
+        });
+
+    }else{
+
+        console.log(nombre);
+        console.log(precio);
+        
+        //Insertar datos a la DB
+        let data = {
+            producto_nombre: nombre, 
+            producto_precio: precio, 
+        }
+
+        let sql = 'Insert into productos set ?';
+
+        conexion.query(sql, data, (error, results) =>{
+            if(error) throw error;
+            res.render('index', {
+                titulo: 'Bienvenidos a Alabama Beer',
+            }); 
+        })
+    }
+});
+
+app.get('/productos', (req, res) =>{
+
+    let sql = 'SELECT * FROM productos';
+
+    conexion.query(sql, (error, results) =>{
+        if(error) throw error;
+        res.render('productos', {
+            titulo: 'MENU',
+            results: results,        
+        })
+    })
+});
+
+app.get('/contacto', (req, res) =>{
+    res.render('contacto', {titulo: 'Escríbenos'})
+});
+
+app.post('/contacto', (req, res) =>{
+
+    //Desestructuración de las variables
+    const { nombre, email } = req.body;
+        
+    if(nombre == "" || email == ""){
+        
+        let validacion = 'Faltan tus datos';
+        
+        res.render('contacto', {
+            titulo: 'Escríbenos',
+            validacion
+        });
+
+    }else{
+        console.log(nombre);
+        console.log(email);
+        res.render('index', {
+            titulo: 'Bienvenidos a Alabama Beer',
+        }); 
+    }
+});
+
+
+app.get('/bebidas', (req, res) =>{
+    res.render('bebidas', {titulo: 'Bebidas'})
+});
+
+//conexion.end();
+
+app.listen(Port, () =>{
+    console.log(`Servidor está trabajando en el Puerto ${Port}`);
+});
+
+app.on('error', (err) =>{
+    console.log(`Error en la ejecución del Servidor ${error}`);
+})
+
